595368bb060687db36e18e2085e7058b720b72f0
gauravsingh2708 <gsnaina2708@gmail.com>
2020-04-26 11:49:49 +0530

    second commit

--- lib/Pages/main_page.dart
+++ lib/Pages/main_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:zuci/Firebase/Authentication.dart';
 import 'package:zuci/Pages/FavouriteList.dart';
 import 'package:zuci/Pages/Messages.dart';
 import 'package:zuci/Pages/Profile.dart';
 import 'package:zuci/Pages/VideoChat.dart';
-import 'package:zuci/Pages/check_auth_page.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -25,70 +23,8 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
+      length: 4,
       child: Scaffold(
-//        appBar: PreferredSize(
-//          child: Container(
-//            decoration: BoxDecoration(
-//                gradient: LinearGradient(
-//                    begin: Alignment.topCenter,
-//                    end: Alignment.bottomCenter,
-//                    stops: [0.2, 1],
-//                    colors: [Colors.purple, Colors.pinkAccent])),
-//            child: SafeArea(
-//                child: Column(
-//              children: <Widget>[
-//                TabBar(
-//                  tabs: [
-//                    Container(
-//                      child: Text(
-//                        'Live',
-//                        style: TextStyle(fontSize: 20.0),
-//                      ),
-//                    ),
-//                    Container(
-//                      child: Text(
-//                        'RANDOM',
-//                        style: TextStyle(fontSize: 20.0),
-//                      ),
-//                    ),
-//                    Container(
-//                      child: Text(
-//                        'NEW',
-//                        style: TextStyle(fontSize: 20.0),
-//                      ),
-//                    ),
-//                  ],
-//                  labelColor: Colors.white,
-//                  indicatorColor: Colors.white,
-//                )
-//              ],
-//            )
-//
-//            ),
-//          ),
-//          preferredSize: Size.fromHeight(32.0),
-//        ),
-//        body:TabBarView(
-//          children: <Widget>[
-//            Column(
-//              children: <Widget>[
-//                Center(child: Text("LIVE")),
-//
-//                )
-//
-//              ],
-//            ),
-//            Column(
-//              children: <Widget>[Center(child: Text("RANDOM"))],
-//            ),
-//            Column(
-//              children: <Widget>[Center(child: Text("NEW"))],
-//            )
-//          ],
-//        ),
-
-
         body: barcount[currentIndex],
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: currentIndex,
