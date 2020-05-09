c7d15fa04ff2425235c1ae3790aabb457faea506
gauravsingh2708 <gsnaina2708@gmail.com>
2020-05-09 12:51:26 +0530

    Merge branch 'temp-branch'
    # Conflicts:
    #	lib/Pages/Messages.dart

--- lib/resources/firebase_methods.dart
+++ lib/resources/firebase_methods.dart
@@ -237,35 +237,6 @@ class FirebaseMethods {
       return null;
     }
   }
-
-//  void setImageMsg(String url, String receiverId, String senderId) async {
-//    Message message;
-//
-//    message = Message.imageMessage(
-//        message: "IMAGE",
-//        receiverId: receiverId,
-//        senderId: senderId,
-//        photoUrl: url,
-//        timestamp: Timestamp.now(),
-//        type: 'image');
-//
-//    // create imagemap
-//    var map = message.toImageMap();
-//
-//    // var map = Map<String, dynamic>();
-//    await firestore
-//        .collection(MESSAGES_COLLECTION)
-//        .document(message.senderId)
-//        .collection(message.receiverId)
-//        .add(map);
-//
-//    firestore
-//        .collection(MESSAGES_COLLECTION)
-//        .document(message.receiverId)
-//        .collection(message.senderId)
-//        .add(map);
-//  }
-
   Future<void> addCoin(uid, addcoins) async {
     print('Add coin is called');
     print(uid);
@@ -325,7 +296,7 @@ class FirebaseMethods {
     _userCollection
         .document(to)
         .collection(HISTORY_COLLECTION)
-        .document(to)
+        .document(of)
         .setData(info);
   }
 
